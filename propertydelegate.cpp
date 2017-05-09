0e80d202099ba98003578ee2a382e0f87b179db2
Tony Rasskazov <tony.rasskazov@gmail.com>
2017-05-09 21:42:13 +0300

    paint method implementation for some classes that have not default QStyledItemDelegate::paint behaviour

--- propertydelegate.cpp
+++ propertydelegate.cpp
@@ -2,6 +2,12 @@
 
 #include <QDebug>
 
+#include <QPainter>
+#include <QPalette>
+
+#include <QByteArray>
+#include <QDataStream>
+
 #include "exvariant.h"
 
 PropertyDelegate::PropertyDelegate(QWidget *parent)
@@ -12,17 +18,43 @@ PropertyDelegate::PropertyDelegate(QWidget *parent)
 
 QString PropertyDelegate::displayText(const QVariant &value, const QLocale &locale) const
 {
-    //qDebug
-
-
     if (value.canConvert<QString>()) {
-        qDebug() << "1" << value << value.toString();
         return QStyledItemDelegate::displayText(value, locale);
     } else {
         ExVariant v(value);
-        qDebug() << "2" << value << v << v.toString();
         return v.toString();
     }
 
+}
+
+void PropertyDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    if (index.data().isNull()) {
+        painter->drawText(option.rect, "[null]", QTextOption(Qt::AlignVCenter | Qt::AlignLeft));
+    }
+
+    QVariant var(index.data());
+
+    if (var.canConvert<QCursor>()) {
+        QCursor c = var.value<QCursor>();
+
+        qDebug() << c.pixmap().isNull()
+                 << c.bitmap()
+                 << c.mask();
+
+        QString s("[cursor]");
+        painter->drawText(option.rect, s, QTextOption(Qt::AlignVCenter | Qt::AlignLeft));
+
+        painter->drawPixmap(0,0,c.pixmap());
+
+    } else if (var.canConvert<QPalette>()) {
+        QPalette p = var.value<QPalette>();
+
+        QString s("[palette]");
+        painter->drawText(option.rect, s, QTextOption(Qt::AlignVCenter | Qt::AlignLeft));
+
+    } else {
+        QStyledItemDelegate::paint(painter, option, index);
+    }
 }
 
--- propertydelegate.h
+++ propertydelegate.h
@@ -9,9 +9,10 @@ public:
     PropertyDelegate(QWidget *parent);
 
     QString displayText(const QVariant &value, const QLocale &locale) const;
-/*
-    QWidget * createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+
+    /*
+    QWidget * createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
     void setEditorData(QWidget *editor, const QModelIndex &index) const;
     void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
     QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
